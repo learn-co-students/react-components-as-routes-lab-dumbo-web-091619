ea58581d0460670bf744fdf0bca879546a483b80
Reina <r.clarkemitchell@gmail.com>
2019-12-23 21:44:01 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import { movies } from '../data';
+import React from "react";
+import { movies } from "../data";
 
-const Movies = () => {
+const Movies = props => {
+  const mapMovies = () => {
+    return movies.map((movie, ind) => {
+      return (
+        <div key={`${movie.title}-${ind}`}>
+          <h2>Movie Title: {movie.title}</h2>
+          <h3> Time: {movie.time} minutes</h3>
+          <h3>Genres:</h3>
+          <ul>
+            {movie.genres.map((genre, idx) => {
+              return <li key={idx}>{genre}</li>;
+            })}
+          </ul>
+        </div>
+      );
+    });
+  };
+
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {mapMovies()}
     </div>
   );
 };
